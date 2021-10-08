55b2cebc0f9961b5c8a7988761abb9de858b4e64
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-08 22:59:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import Modal from './components/Modal';
+import Button from './components/Button';
+import IconButton from './components/IconButton';
+
+import { ReactComponent as CloseIcon } from './icons/closeBtnIcon.svg';
 
 import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <>
+        <Button onClick={this.toggleModal} label="Open modal" />
+
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <IconButton onClick={this.toggleModal}>
+              <CloseIcon width="40" height="40" />
+            </IconButton>
+            <img
+              src="https://cdn.pixabay.com/photo/2021/08/25/17/22/flowers-6574079_1280.jpg"
+              alt=""
+            />
+          </Modal>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
