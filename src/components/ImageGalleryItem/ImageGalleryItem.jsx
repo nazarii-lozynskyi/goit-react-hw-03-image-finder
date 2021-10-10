d0c0b8ee31be8075dcf4817ba5b015979a068b45
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-10 18:42:34 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,3 +1,17 @@
-import React from 'react';
+import { Component } from 'react';
 
 import styles from './ImageGalleryItem.module.css';
+
+export default class ImageGalleryItem extends Component {
+  render() {
+    return (
+      <li className={styles.Item}>
+        <img
+          src={this.props.src}
+          alt={this.props.alt}
+          className={styles.Image}
+        />
+      </li>
+    );
+  }
+}
