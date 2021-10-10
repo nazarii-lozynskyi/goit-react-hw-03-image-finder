0a6795a00ba0f08283858499441c528f6db57479
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-10 18:41:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Searchbar from './components/Searchbar';
+import ImageGallery from './components/ImageGallery';
+
 import Modal from './components/Modal';
 import Button from './components/Button';
 import IconButton from './components/IconButton';
@@ -8,7 +14,13 @@ import { ReactComponent as CloseIcon } from './icons/closeBtnIcon.svg';
 
 class App extends Component {
   state = {
+    inputValue: '',
     showModal: false,
+    loading: false,
+  };
+
+  handleSearchbarSubmit = inputValue => {
+    this.setState({ inputValue });
   };
 
   toggleModal = () => {
@@ -17,10 +29,26 @@ class App extends Component {
     }));
   };
 
+  loadMore() {}
+
   render() {
     const { showModal } = this.state;
     return (
       <>
+        <Searchbar onSubmit={this.handleSearchbarSubmit} />
+        <ToastContainer
+          theme="colored"
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick={true}
+          pauseOnHover={true}
+          draggable={true}
+          progress={undefined}
+        />
+
+        <ImageGallery inputValue={this.state.inputValue} />
+
         <Button onClick={this.toggleModal} label="Open modal" />
 
         {showModal && (
