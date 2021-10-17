2d158dd6720402ab9e992925056710ba52900dd2
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-17 15:54:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,39 @@ import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Container from './components/Container';
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
 
-import Modal from './components/Modal';
-import Button from './components/Button';
-import IconButton from './components/IconButton';
-
-import { ReactComponent as CloseIcon } from './icons/closeBtnIcon.svg';
+// import Modal from './components/Modal';
+// import Image from './components/Image';
+// import IconButton from './components/IconButton';
+// import { ReactComponent as CloseIcon } from './icons/closeBtnIcon.svg';
 
 class App extends Component {
   state = {
     inputValue: '',
     showModal: false,
-    loading: false,
   };
 
   handleSearchbarSubmit = inputValue => {
-    this.setState({ inputValue });
+    this.setState({ inputValue, page: 1 });
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
+  // toggleModal = () => {
+  //   this.setState(({ showModal }) => ({
+  //     showModal: !showModal,
+  //   }));
+  // };
 
   loadMore() {}
 
   render() {
-    const { showModal } = this.state;
+    // const { showModal } = this.state;
     return (
       <>
         <Searchbar onSubmit={this.handleSearchbarSubmit} />
+
         <ToastContainer
           theme="colored"
           position="top-right"
@@ -47,21 +47,19 @@ class App extends Component {
           progress={undefined}
         />
 
-        <ImageGallery inputValue={this.state.inputValue} />
+        <Container>
+          <ImageGallery inputValue={this.state.inputValue} />
+        </Container>
 
-        <Button onClick={this.toggleModal} label="Open modal" />
-
-        {showModal && (
+        {/* {showModal && (
           <Modal onClose={this.toggleModal}>
             <IconButton onClick={this.toggleModal} aria-label="Close Button">
               <CloseIcon width="40" height="40" />
             </IconButton>
-            <img
-              src="https://cdn.pixabay.com/photo/2021/08/25/17/22/flowers-6574079_1280.jpg"
-              alt="flower"
-            />
+
+            <Image la />
           </Modal>
-        )}
+        )} */}
       </>
     );
   }
